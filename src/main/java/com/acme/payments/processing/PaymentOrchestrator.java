6f1266c73c83ad63cc2f489e1bdce91a97cc8033
imusa654 <imusa@nibss-plc.com.ng>
2025-08-23 20:12:18 +0100

    processor

---
+++ src/main/java/com/acme/payments/processing/PaymentOrchestrator.java
@@ -0,0 +1,11 @@
+package com.acme.payments.processing;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class PaymentOrchestrator {
+
+}
